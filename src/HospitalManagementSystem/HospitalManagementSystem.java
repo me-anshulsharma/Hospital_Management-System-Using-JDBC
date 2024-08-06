74789788ebaf2d6009849c3bb607f14d9e9453bc
meetwithanshul <work.anshulsharma@gmail.com>
2024-08-06 17:37:03 +0530

    Initial commit

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -1,5 +1,5 @@
 package HospitalManagementSystem;
-
+//Importing libraries
 import java.sql.*;
 import java.util.Scanner;
 import java.util.logging.Level;
--- src/HospitalManagementSystem/Patient.java
+++ src/HospitalManagementSystem/Patient.java
@@ -1,5 +1,5 @@
 package HospitalManagementSystem;
-
+//Importing libraries
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
